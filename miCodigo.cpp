3b97b86a335b218339e53206e3a671ba42894a8f
Daniel Carrizo <danjo2c@gmail.com>
2024-10-04 14:06:59 -0400

    refactor: funcion suma

--- miCodigo.cpp
+++ miCodigo.cpp
@@ -1,12 +1,12 @@
 #include<iostream>
 using namespace std;
 
-int sum(int a, int b){
-    return a+b;
+int sum(int a, int b, int c){
+    return a+b+c;
 }
 
 int main(){
     cout<<"mi codigo"<<endl;
-    cout<<sum(4,4)<<endl;
+    cout<<sum(4,4,4)<<endl;
     return 0;
 }
